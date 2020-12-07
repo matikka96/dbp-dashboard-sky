effc79a8c37b732bb4ed78875a713d2f80256078
Aaro Hassinen <aaro.hassinen@workseed.fi>
2020-12-07 13:36:15 +0200

    CORS flask

--- backend/app/flask_app/app.py
+++ backend/app/flask_app/app.py
@@ -3,9 +3,10 @@ from flask import Flask, jsonify, make_response
 from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
